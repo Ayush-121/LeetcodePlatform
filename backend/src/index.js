a0714662b16e5b90182d5afcae4e894df2f0683e
AYUSH KUMAR <ayushkumarworld0@gmail.com>
2026-07-27 22:58:41 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,10 @@ const cors = require('cors')
 // console.log("Hello")
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173',
+             'https://leetcode-platform.vercel.app/signup'
+             ],
+    
     credentials: true 
 }))
 
